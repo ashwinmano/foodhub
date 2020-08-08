90d97442692b2d8f7e77abc0e682b33cbe7c01d4
Ashwinmano <ashwin.m@sceince.christuniversity.in>
2020-08-08 15:11:56 +0530

    END PRODUCT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useState} from 'react';
+import Axios from 'axios';
+import {v4 as uuidv4} from 'uuid';
+import Recipe from './components/Recipe';
 import './App.css';
+import Alert from './components/Alert';
 
 function App() {
+  const[query,setquery]=useState("");
+  const[recipes,setRecipes]=useState([]);
+  const[alert,setAlert]=useState("");
+  const APP_ID="caaf70cd";
+  const APP_KEY="3a4a00a75a9891e37feed9a10daaf8fd	";
+  const url=`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
+  const getData =async() =>{
+    if (query!== ""){
+      const result=await Axios.get(url);
+      if (!result.data.more){
+        return setAlert("No food with such name");
+      }
+      setRecipes(result.data.hits)
+      console.log(result)
+      setAlert("");
+      setquery("");
+    }
+    else{
+      setAlert("Please enter the food");
+    }
+    
+  }
+
+  const onChange =e =>{
+    setquery(e.target.value);
+  }
+
+  const onSubmit =(e) =>{
+    e.preventDefault();
+    getData();
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 >Food search App</h1>
+      <form className="search-form" onSubmit={onSubmit}>
+        {alert !=="" && <Alert alert={alert}/>}
+        <input type="text" placeholder="Search Your Food " 
+        autoComplete="off" onChange={onChange} value={query} />
+        <input type="submit" value="search" />
+      </form>
+      <div className="recipes">
+        {recipes !==[] && 
+        recipes.map(recipe =><Recipe key={uuidv4} recipe={recipe}/>)}
+      </div>
     </div>
   );
 }
---
+++ src/components/Alert.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Alert =({alert}) =>{
+    return(
+        <div className="alert">
+            {alert}
+        </div>
+    );
+};
+
+export default Alert;
